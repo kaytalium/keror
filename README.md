c843c7755254b4cee5c388ebbb4c61c822f5ac05
Ovel Heslop <kaytalium@Kynmbus-MacBook-Pro.local>
2017-08-25 13:59:07 -0500

    update project

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-##Keror
+# Keror
 
 This application allow users to predict the future by uploading thier data for the system to analyst and map the prediction from, the more data you feed the program the better the prediction. 
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict'
 const electron = require('electron')
 // Module to control application life.
 const app = electron.app
@@ -8,12 +9,15 @@ const BrowserWindow = electron.BrowserWindow
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+//Development mode only 
+require('electron-reload')(__dirname);
+
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
