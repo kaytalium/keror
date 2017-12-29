5fe928d2c32c1440614c747d9159952e20979034
Ovel Heslop <kaytalium@Kynmbus-MacBook-Pro.local>
2017-12-29 11:12:43 -0500

    update to reflect changes made to clock module

--- app/scripts/interface.ts
+++ app/scripts/interface.ts
@@ -3,12 +3,24 @@ export interface User{
     password:any
 }
 
-export interface activeUser {
+export interface ActiveUser {
     image: String,
     fullname: String,
     firstname: String,
     lastname: String,
     status?: String,
     icon?: String,
+    username: String,
+    isSwitching?:boolean,
+    background_theme:boolean,
+    background_url:String
 
+}
+
+export interface TimeState{
+    mid: String,
+    hrs: number,
+    min: string | number,
+    sec: string | number,
+    fullTime: string
 }
