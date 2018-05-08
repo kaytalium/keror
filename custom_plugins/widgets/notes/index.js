67d74a39923be1fa0670ecdae1c54afc8990cd77
Ovel Heslop <kayheslop@gmail.com>
2018-05-08 13:11:40 -0500

    Delete index.js

--- custom_plugins/widgets/notes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var notes;
-(function (notes_1) {
-    let notes = {
-        name: "John Doe",
-        mesg: "Welcome to Bamboo town"
-    };
-})(notes = exports.notes || (exports.notes = {}));
