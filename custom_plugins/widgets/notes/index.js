3c10c7118330e6c59fc9957875290157e8bbd032
Ovel Heslop <kayheslop@gmail.com>
2018-05-08 13:07:42 -0500

    Delete index.js

--- custom_plugins/widgets/notes/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var notes;
-(function (notes) {
-    let note = { name: "John" };
-})(notes = exports.notes || (exports.notes = {}));
