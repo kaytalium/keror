9f97b257dbdf3a0eda1fb87a8814015f1bf32a19
Ovel Heslop <kayheslop@gmail.com>
2018-05-08 13:14:44 -0500

    update changes to time

--- custom_plugins/widgets/notes/index.ts
+++ custom_plugins/widgets/notes/index.ts
@@ -2,7 +2,8 @@ export module notes{
     let notes = {
         name:"John Doe",
         mesg: "Welcome to Bamboo town",
-        age:12
+        age:12,
+        time:1475847854547
     };
      
 }
